bc6805c5d81a7b98f5f2d9df32088756e9d83bd3
krakenon <luonguit@gmail.com>
2021-11-18 09:04:56 +0700

    init

---
+++ index.js
@@ -0,0 +1,53 @@
+(function () {
+  "use strict";
+
+  var mutationObserver =
+    window.MutationObserver ||
+    window.WebKitMutationObserver ||
+    window.MozMutationObserver;
+
+  function removeContentSponsors() {
+    const feedItems = document.querySelectorAll(
+      'div[role="feed"] > div[data-pagelet]'
+    );
+    feedItems.forEach((feed) => {
+      const sponsorElm = feed.querySelector('a[aria-label="Sponsored"]');
+      if (sponsorElm) {
+        console.log("===> remove sponsored content...");
+        feed.remove();
+      }
+    });
+  }
+
+  function removePanelSponsors() {
+    const feedItems = document.querySelectorAll(
+      'div[data-pagelet="RightRail"] > div'
+    );
+
+    feedItems.forEach((feed) => {
+      const sponsorElm = feed.querySelector("a");
+      if (sponsorElm && /\/gaming\/.*/g.exec(sponsorElm.href)) {
+        console.log("===> remove sponsored gaming: ", sponsorElm.href);
+        feed.remove();
+      }
+    });
+  }
+
+  function process() {
+    removeContentSponsors();
+    removePanelSponsors();
+  }
+
+  if (mutationObserver) {
+    var body = document.querySelector("body");
+    if (!body) {
+      return;
+    }
+
+    var observer = new mutationObserver(process);
+    observer.observe(body, {
+      childList: true,
+      subtree: true,
+    });
+  }
+})();
